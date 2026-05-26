fbe707a2d496def346003b7b31f9db4a8529a6e2
Sohil Mahto <52059603+sohilmahto0410@users.noreply.github.com>
2026-05-27 01:08:02 +0530

    Update derpfest.sh

--- derpfest.sh
+++ derpfest.sh
@@ -3,36 +3,39 @@
 rm -rf .repo/local_manifests/
 
 # Local TimeZone
-sudo rm -rf /etc/localtime
-sudo ln -s /usr/share/zoneinfo/Asia/Dhaka /etc/localtime
+# sudo rm -rf /etc/localtime
+# sudo ln -s /usr/share/zoneinfo/Asia/Dhaka /etc/localtime
 
 # Rom source repo
-repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16.2 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16.2 --git-lfs 
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Clone local_manifests repository
-git clone https://github.com/sohilmahto0410/local_manifests_camellia.git .repo/local_manifests
-echo "============================"
-echo "Local manifest clone success"
-echo "============================"
+# git clone https://github.com/sohilmahto0410/local_manifests_camellia.git .repo/local_manifests
+# echo "============================"
+# echo "Local manifest clone success"
+# echo "============================"
 
 # Sync the repositories
 /opt/crave/resync.sh
 echo "============================"
 
-# Remove Project
-rm -rf rm -rf hardware/mediatek && rm -rf device/mediatek/sepolicy_vndr && rm -rf hardware/xiaomi 
-echo "======= Remove Done ======"
+# # Remove Project
+# rm -rf rm -rf hardware/mediatek && rm -rf device/mediatek/sepolicy_vndr && rm -rf hardware/xiaomi 
+# echo "======= Remove Done ======"
+
+git clone https://github.com/sohilmahto0410/device_xiaomi_camellia.git device/xiaomi/camellia
+git clone https://github.com/aLpHa-Git-69/vendor_xiaomi_camellia.git vendor/xiaomi/camellia
+git clone https://github.com/dm700-devs/device_xiaomi_camellia-kernel.git device/xiaomi/camellia-kernel
+git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_hardware_mediatek.git hardware/mediatek
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
 
 
 
-# Export
-export BUILD_USERNAME= sohilmahto
-export BUILD_HOSTNAME=crave
-export TARGET_HAS_UDFPS=true
-echo "======= Export Done ======"
 
 # Set up build environment
 source build/envsetup.sh
@@ -42,9 +45,9 @@ echo "====== Envsetup Done ======="
 lunch lineage_camellia-bp4a-user
 echo "============="
 
-# Make cleaninstall
-make installclean
-echo "============="
+# # Make cleaninstall
+# make installclean
+# echo "============="
 
 # Build rom
 mka derp
