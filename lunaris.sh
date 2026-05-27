65b01fe945bc5c37d895893c961f4878cf7903fd
Sohil Mahto <52059603+sohilmahto0410@users.noreply.github.com>
2026-05-27 20:22:03 +0530

    Add files via upload

---
+++ lunaris.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+
+# Rom source repo
+repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
+
+
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+echo "===== Sync the repositories ========"
+
+/opt/crave/resync.sh
+
+echo "======== Syncing Done =========="
+
+
+
+echo "======= Remove Project ========="
+# # 
+rm -rf .repo/local_manifests
+rm -rf kernel/xiaomi
+rm -rf device/xiaomi
+rm -rf device/mediatek/sepolicy_vndr
+rm -rf hardware/xiaomi
+rm -rf vendor/xiaomi
+rm -rf vendor/mediatek
+
+
+
+echo "======= Remove Done ======"
+
+git clone --depth=1 https://github.com/sohilmahto0410/device_xiaomi_camellia.git -b lunaris device/xiaomi/camellia
+git clone --depth=1 https://github.com/aLpHa-Git-69/vendor_xiaomi_camellia.git vendor/xiaomi/camellia
+git clone --depth=1 https://github.com/dm700-devs/device_xiaomi_camellia-kernel.git device/xiaomi/camellia-kernel
+git clone --depth=1 https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
+git clone --depth=1 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
+git clone --depth=1 https://github.com/LineageOS/android_hardware_mediatek.git hardware/mediatek
+git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+
+
+echo "=================="
+echo "Cloning Done"
+echo "=================="
+
+#Exports
+export WITH_GMS=false
+export TARGET_SUPPORTS_GOOGLE_TELEPHONY=false
+export SURFACE_FLINGER_BOOST=true
+
+
+echo "=================="
+echo " Exports Done "
+echo "=================="
+
+
+# Set up build environment
+. b*/env*
+echo "====== Envsetup Done ======="
+
+
+# Lunch
+lunch lineage_camellia-bp4a-user
+echo "============="
+
+# # Make cleaninstall
+make installclean
+# echo "============="
+
+echo "=================="
+echo "Building Rom"
+echo "=================="
+# Build rom
+m bacon
