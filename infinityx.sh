c9223c774e7dfa0d11e8642941f72ba95dcd3847
Sohil Mahto <52059603+sohilmahto0410@users.noreply.github.com>
2026-06-02 20:39:53 +0530

    Update infinityx.sh

--- infinityx.sh
+++ infinityx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+#repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 echo "======= Remove Project ========="
