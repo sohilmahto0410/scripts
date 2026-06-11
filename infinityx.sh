a0040c880c0d36b1525285e6547a4f7f2779bee8
Sohil Mahto <52059603+sohilmahto0410@users.noreply.github.com>
2026-06-11 12:24:54 +0530

    Update infinityx.sh

--- infinityx.sh
+++ infinityx.sh
@@ -1,56 +1,75 @@
 #!/bin/bash
 
 #repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 echo "======= Remove Project ========="
-# # 
-# rm -rf .repo/local_manifests
-# rm -rf kernel/xiaomi
-# rm -rf device/xiaomi
-# rm -rf device/mediatek/sepolicy_vndr
-# rm -rf hardware/xiaomi
-# rm -rf vendor/xiaomi
-# rm -rf vendor/mediatek
+# 
+rm -rf .repo/local_manifests
+rm -rf kernel/xiaomi
+rm -rf device/xiaomi
+rm -rf device/mediatek/sepolicy_vndr
+rm -rf hardware/xiaomi
+rm -rf vendor/xiaomi
+rm -rf vendor/mediatek
+
+# Rom repo init
+
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+
+echo "-----------------------------"
+echo "Repo init cloned successfully"
+echo "-----------------------------"
+
+# sycning
+
+echo "-----------------------"
+echo "Starting to sync source"
+echo "-----------------------"
+
+/opt/crave/resync.sh
 
 
 
-echo "======= Remove Done ======"
-
-git clone --depth=1 https://github.com/sohilmahto0410/device_xiaomi_camellia.git -b infinityx  device/xiaomi/camellia
-git clone --depth=1 https://github.com/aLpHa-Git-69/vendor_xiaomi_camellia.git vendor/xiaomi/camellia
-git clone --depth=1 https://github.com/dm700-devs/device_xiaomi_camellia-kernel.git device/xiaomi/camellia-kernel
-git clone --depth=1 https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
-git clone --depth=1 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
-git clone --depth=1 https://github.com/LineageOS/android_hardware_mediatek.git hardware/mediatek
-git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+echo "------------------------"
+echo "Source syncing comppeted"
+echo "------------------------"
 
 
-echo "=================="
-echo "Cloning Done"
-echo "=================="
+# Dt
+
+git clone https://github.com/aLpHa-Git-69/device_xiaomi_camellia.git -b infinityx device/xiaomi/camellia
+git clone https://github.com/aLpHa-Git-69/vendor_xiaomi_camellia.git vendor/xiaomi/camellia
+git clone https://github.com/dm700-devs/device_xiaomi_camellia-kernel.git device/xiaomi/camellia-kernel
+git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_hardware_mediatek.git hardware/mediatek
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+
+echo "---------------------"
+echo "Tress clone completed"
+echo "---------------------"
+
+# build env
+
+. b*/e*
+
+echo "---------------------------"
+echo "Build/envsetup.sh completed"
+echo "---------------------------"
+
+# signing script
 
 
+echo "----------------------------"
+echo "Rom signed with private keys"
+echo "----------------------------"
 
-echo "=================="
-echo " Exports Done "
-echo "=================="
+# boss
 
+lunch infinity-camellia-user
 
-# Set up build environment
-. b*/env*
-echo "====== Envsetup Done ======="
+m bacon
 
-
-# Lunch
-lunch infinity_camellia-userdebug
-echo "============="
-
-
-echo "=================="
-echo "Building Rom"
-echo "=================="
-# Build rom
-make installclean
-
-m bacon 
+echo "----------"
+echo "Done"
+echo "----------"
